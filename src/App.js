6caef23de465fc7e2c80a7dda5532efb5adc5634
Martin Chak <Martin.Chak@mail.utoronto.ca>
2020-06-11 04:19:18 -0400

    Finished header and intro

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+/** Import library **/
 import React from 'react';
+
+/** Import components **/ 
 import Navbar from './Navbar.js';
 import Courses from './Courses.js';
 import Skills from './Skills.js';
 import Projects from './Projects.js';
+
+/** Import images **/
+import headerImage from '../src/assets/header-image.jpg'
+
+/** Import Styles **/
 import './styles/App.css';
 
 function App() {
@@ -10,9 +18,31 @@ function App() {
     <div className="App">
       <Navbar/>
       <div id="Header">
-
+        <img id="Header-picture" src={headerImage} alt="Missing asset"/>
+        <header id="Header-title">
+          Martin Chak
+        </header>
+        <p id="Header-text">
+          Third Year Computer Science Student <br/> And Aspiring Software Developer
+        </p>
       </div>
       <div id="Intro">
+        <div id="Intro-header">
+          Introduction
+        </div>
+        <div id="Intro-text">
+          I am a student studying for a Computer Science specialist with a double major in Mathematics at 
+          the University of Toronto. I started programming late into high school and now do it as both a 
+          hobby and future profession.
+          <br/><br/>
+          I'm currently looking for 2020-2022 PEY co-op positions at the moment, if you're interested you
+          can view my resume <a href="https://drive.google.com/open?id=1stbWlfTAVCdx1MCJwrVb7PcRH38nwYNw" className="text-link">here.</a>
+        </div>
+      </div>
+      <div id="Education">
+
+      </div>
+      <div id="Courses">
 
       </div>
       <Courses/>
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,13 @@ import './styles/Navbar.css';
 function Navbar() {
   return (
     <div id="Navbar">
-      
+      <span id="Navbar-header">Martin Chak</span>
+      <div id="Navbar-fields">
+        <p>Home</p>
+        <p>About</p>
+        <p>Projects</p>
+        <p>Contacts</p>
+      </div>
     </div>
   );
 }
